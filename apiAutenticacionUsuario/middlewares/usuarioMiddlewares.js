c0a76828242a6e71499580424fae44e37d423c02
Torres-jose <jose-torres-soto@outlook.es>
2025-06-21 00:24:37 -0500

    suebiendo que hace falta

---
+++ apiAutenticacionUsuario/middlewares/usuarioMiddlewares.js
@@ -0,0 +1,20 @@
+/*
+API de Autenticación de Usuarios
+Construye una API en Express.js para autenticar usuarios. El sistema debe:
+- Registrar usuarios con email y password en memoria (POST /auth/register)
+- Iniciar sesión validando credenciales (POST /auth/login)
+- Manejar errores de registro (email duplicado) y login (credenciales inválidas)
+- Validar formato de email y longitud mínima de password 
+*/
+const validarUsuario = (req, res, next) => {
+    const {email, password} =req.body;
+
+    if(!email || !password){
+        return res.status(400).json({Error: "Debe ingresar su correo o cotreseña "});
+        
+    }
+
+    
+}
+
+
--- apiAutenticacionUsuario/models/autenticadorModels.js
+++ apiAutenticacionUsuario/models/autenticadorModels.js
@@ -6,3 +6,22 @@ Construye una API en Express.js para autenticar usuarios. El sistema debe:
 - Manejar errores de registro (email duplicado) y login (credenciales inválidas)
 - Validar formato de email y longitud mínima de password 
 */
+
+const fs = require('fs');
+const path = require('path');
+
+
+const rutaArchivo = path.join(__dirname, '../data/bdUsuario.json');
+
+const leerUsuario = () => {
+    const data = fs.readFileSync(rutaArchivo, 'utf8');
+    return JSON.parse(data);
+};
+
+const guardarUsuario = (usuario) =>{
+    fs.writeFileSync(rutaArchivo, JSON.stringify(usuario, null , 2));
+}; 
+
+module.exports = {
+    leerUsuario, guardarUsuario
+}
